344c7f88eea20e11e78896bab98e5e183c015c5d
Anastasiya Mar <nastenkammmggg@gmail.com>
2024-10-08 17:53:07 +0300

    solved and added task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("you are cutie");
+﻿Console.WriteLine("vvedi naturalnoe trehzn chislo");
+int num=Convert.ToInt32(Console.ReadLine());
+int a=num/100;
+int c=num%10;
+int newnum=a*10+c;
+Console.WriteLine($"{newnum} - novoe chislo");
--- task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ac4992358a5d1bab2bb132f4c02c4dfd2fde714")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
