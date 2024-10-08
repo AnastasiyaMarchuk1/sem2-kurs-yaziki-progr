281b0f27e8dcd4defa33ba3e05778123bdf782bf
Anastasiya Mar <nastenkammmggg@gmail.com>
2024-10-08 19:08:02 +0300

    fially solved task2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("vvedi naturalnoe trehzn chislo");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int seconddigit = num / 10 % 10;
+int thirddigit = num % 10;
+int i = 1;
+int result = seconddigit;
+while (i < thirddigit)
+{
+    result = result * seconddigit;
+    i++;
+}
+Console.WriteLine($"{result} - resultat");
